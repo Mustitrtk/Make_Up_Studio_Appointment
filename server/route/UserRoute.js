52255dc21cdff73a55abb49d335c42eebc71b85a
Mustafa TORTUK <mustafat237@hotmail.com>
2025-08-02 19:28:37 +0300

    Template added.

--- server/route/UserRoute.js
+++ server/route/UserRoute.js
@@ -3,6 +3,8 @@ import UserController from '../controller/UserController.js';
 
 const router = express.Router();
 
+router.get('/TKdbV4HeVsSFZ8wheFDTA37ko/login', UserController.login);
+
 router.post('/TKdbV4HeVsSFZ8wheFDTA37ko/login', UserController.login);
 
 export default router;
